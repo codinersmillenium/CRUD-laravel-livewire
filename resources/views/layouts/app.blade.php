18ac53f07124444e909379450f13e38e2bc4694b
codinersmillenium <rizqidsas@gmail.com>
2021-12-23 23:25:36 +0700

    example SPA livewire

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Laravel Livewire SPA - SantriKoding.com</title>
+    <title>Example Livewire SPA</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
     <livewire:styles/>
     <livewire:scripts/>
@@ -23,7 +23,7 @@
                 <!-- end flash message -->
 
                 <!-- component -->
-                @yield('content’)
+                @yield('content')
                 <!-- end component -->
 
             </div>
